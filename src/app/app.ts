438b25c147c4a6ce957b41c089aff34876b5f057
Fabricio Garcia <fabriciojosuegarciapena@gmail.com>
2026-02-17 20:53:40 -0600

    feat:add componente galery

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Hero } from "./components/hero/hero";
 import { BigDay } from "./components/big-day/big-day";
+import { Galery } from "./components/galery/galery";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Hero, BigDay],
+  imports: [RouterOutlet, Hero, BigDay, Galery],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
