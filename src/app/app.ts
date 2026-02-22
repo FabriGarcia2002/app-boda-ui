5bcf043d00efb280c8670d478b258955759ee2a2
Fabricio Garcia <fabriciojosuegarciapena@gmail.com>
2026-02-22 17:38:08 -0600

    feat: complete footer functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Hero } from "./components/hero/hero";
 import { BigDay } from "./components/big-day/big-day";
 import { Galery } from "./components/galery/galery";
 import { EventDetails } from "./components/event-details/event-details";
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Hero, BigDay, Galery, EventDetails],
+  imports: [RouterOutlet, Hero, BigDay, Galery, EventDetails, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
